b4964e12c88915753f279bee08cd5ac799ad2fdf
eapenm <eapenmani@gmail.com>
2023-10-30 12:44:42 +0000

    Implemented lifecycle meta tags

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -34,6 +34,11 @@ resource "aws_s3_object" "index_html" {
   content_type = "text/html"
 
   etag = filemd5(var.index_html_filepath)
+
+    lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
@@ -44,6 +49,11 @@ resource "aws_s3_object" "error_html" {
   content_type = "text/html"
 
   etag = filemd5(var.error_html_filepath)
+
+    lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -67,4 +77,9 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input= var.content_version
+  
 }
