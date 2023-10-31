5e8539871fcd1fd8ed5af0baa3c82459bd328b03
Eapen Mani <eapenmani@gmail.com>
2023-10-31 07:06:46 +0530

    Updated the terraform files for invalidate cache in CDN

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,14 @@
 output "bucket_name" {
-  description = "Bucket name for our static website hosting"  
-  value = module.terrahouse_aws.bucket_name
+  description = "Bucket name for our static website hosting"
+  value       = module.terrahouse_aws.bucket_name
 }
 
 output "s3_website_endpoint" {
-  description = "Endpoint of hosted static end point"  
-  value = module.terrahouse_aws.website_endpoint
+  description = "Endpoint of hosted static end point"
+  value       = module.terrahouse_aws.website_endpoint
 }
 
+output "cloudfront_url" {
+  description = "The cloudfront distribution Domain"
+  value       = module.terrahouse_aws.cloudfront_url
+}
